7a921e785eb034432ea5518dffd4f9be44714b56
Vasili Mandalac <vasilimandalac@Vasilis-MacBook-Pro.local>
2020-05-07 01:50:33 -0500

    initial

---
+++ docs/precache-manifest.852b630276c93b5605f231ce9024c263.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35dfb9b1c7240e17d8566700cefb0209",
+    "url": "/prop-it-up/index.html"
+  },
+  {
+    "revision": "d2bd99cccba4ffc5b226",
+    "url": "/prop-it-up/static/css/main.314e58cf.chunk.css"
+  },
+  {
+    "revision": "c518d5781f4246d1f7f9",
+    "url": "/prop-it-up/static/js/2.78810e48.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/prop-it-up/static/js/2.78810e48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2bd99cccba4ffc5b226",
+    "url": "/prop-it-up/static/js/main.61e5a62e.chunk.js"
+  },
+  {
+    "revision": "d4f060ba53f68fed2a76",
+    "url": "/prop-it-up/static/js/runtime-main.175975c5.js"
+  }
+]);
